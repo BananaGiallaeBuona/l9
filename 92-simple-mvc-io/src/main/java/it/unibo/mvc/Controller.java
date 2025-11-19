83b70388edb7695f8ffb52bb90e4f52b776a66c3
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-11-19 12:41:14 +0100

    thos files works until 92.2, they need to be verified for build

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,65 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public final class Controller {
+    private File file;
+    private static final String SEP = System.getProperty("file.separator");
+    private static final String PROJECT_ROOT = System.getProperty("user.dir");
+    private static final File DEFAULT_FILE =
+        new File(PROJECT_ROOT + SEP + "build" + SEP + "texts" + SEP + "output.txt");
+
+    Controller() {
+        this.file = DEFAULT_FILE;
+        this.file.getParentFile().mkdirs(); //this creates al the directory that doesn't
+                                            //already exists
+    }
+
+    /**
+     * Sets the current file used for I/O operations.
+     *
+     * @param file the file to use
+     */
+    public void setFile(final File file) {
+        this.file = file;
+    }
+
+    /**
+     * gets the current file used for I/O operations.
+     *
+     * @return  this.file 
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * gets the path of the file used for I/O operations.
+     *
+     * @return  the path of the file used for I/O operations.
+     */
+    public Path getFilePath() {
+        return this.file.toPath();
+    }
+
+    /**
+     * Writes the given content to the current file.
+     * The content replaces any existing data in the file.
+     *
+     * @param content the text to write
+     * @throws IOException if an I/O error occurs while writing
+     */
+    public void write(final String content) throws IOException {
+        try (PrintStream ps = new PrintStream(this.file, StandardCharsets.UTF_8)) {
+            ps.print(content);
+        }
+    }
 
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,78 @@
 package it.unibo.mvc;
 
+//import it.unibo.mvc.Controller;
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
+    private static Controller controller = new Controller();
+    private final static int PROPORTION = 5;
+    private static final String TITLE = "EX 92";
+    private final JFrame frame = new JFrame(TITLE);
+    private  JTextArea text;
+    private  JButton saveButton;
 
-    private final JFrame frame = new JFrame();
+    SimpleGUI(){
+        JPanel canvas = new JPanel(new BorderLayout());
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.text = new JTextArea("write here");
+        //controller.setFile(this.text);
+        canvas.add(text, BorderLayout.CENTER);
+        this.saveButton = new JButton(new ImageIcon("src/test/resources/ex02.png"));
+        canvas.add(saveButton, BorderLayout.LINE_END);
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ignored) {
+                try {
+                    controller.write(text.getText());
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
+            }
+        });
+    }
 
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 }
